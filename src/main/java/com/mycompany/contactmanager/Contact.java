4a61e3df399d84bc801c3d7a5d8456d2206f01dd
defranjo <joe.defranco@sheridancollege.ca>
2024-11-19 19:56:18 -0500

    My first comment

---
+++ src/main/java/com/mycompany/contactmanager/Contact.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.contactmanager;
+
+/**
+ *
+ * @author joede
+ */
+public class Contact {
+
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+   
+}
